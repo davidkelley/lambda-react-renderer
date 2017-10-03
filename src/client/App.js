f7272c51cca7626b2173955f0996100b91945dc9
davidkelley <david.james.kelley@gmail.com>
2017-10-03 12:20:24 +0100

    simplifying client app

--- src/client/App.js
+++ src/client/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Switch, Link, Route } from 'react-router-dom';
 
-import Topics from './views/Topics';
+import Home from './views/Home';
+import Contact from './views/Contact';
 import About from './views/About';
 
 // Use this as the entry-point for your app.
@@ -10,15 +11,15 @@ export default () => (
     <ul>
       <li><Link to="/">Home</Link></li>
       <li><Link to="/about">About</Link></li>
-      <li><Link to="/topics">Topics</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
     </ul>
 
     <hr/>
 
     <Switch>
-      <Route exact path="/" component={Topics} />
+      <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
+      <Route path="/contact" component={Contact} />
     </Switch>
   </div>
 );
--- src/client/views/Topic.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default ({ params }) => (
-  <div>
-    <h3>{params.topicId}</h3>
-  </div>
-);
